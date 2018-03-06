4283b93cafbd48338a343a0d8e23c962601ec871
Karpowicz <Emil.Karpowicz@unilever.com>
2018-03-06 16:58:18 +0100

    Initial commit

---
+++ src/compute/network/and/broadcast/Main.java
@@ -0,0 +1,15 @@
+package compute.network.and.broadcast;
+
+/**
+ * Pro/g/ramming Challenges v 3.0
+ * 005 - Given an IPv4 address and a subnet mask, compute the network, broadcast and first/last host addreses
+ * @author Emil.Karpowicz
+ */
+public class Main 
+{
+
+    public static void main(String[] args) 
+    {
+    }
+    
+}
